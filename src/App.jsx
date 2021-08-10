28aac3ea190f5d1b34b2927a7f5e8a215f89a928
bk20dev <60577942+bk20dev@users.noreply.github.com>
2021-08-10 19:17:48 +0200

    Added Loader. Fixed styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { ScaleLoader } from 'react-spinners';
 import styled from 'styled-components';
 import Gallery from './features/gallery/Gallery';
 import Footer from './features/navigation/Footer';
@@ -9,6 +10,12 @@ const StyledContainer = styled.div`
   padding: 0 1.75rem;
   max-width: 81rem;
   margin: auto;
+  height: 100%;
+`;
+
+const LoaderWrapper = styled.div`
+  margin: 4rem auto;
+  width: max-content;
 `;
 
 const App = () => {
@@ -16,9 +23,15 @@ const App = () => {
 
   const photos = useSearchPhotos(data);
 
+  const renderLoader = () => (
+    <LoaderWrapper>
+      <ScaleLoader />
+    </LoaderWrapper>
+  );
+
   const renderGallery = () => {
     if (error) return <h3>Oops! There was an error</h3>;
-    if (isLoading) return <h3>Loading</h3>;
+    if (isLoading) return renderLoader();
     if (data.length && !photos.length) return <h3>No results</h3>;
     return <Gallery photos={photos} />;
   };
